56df08b0a9ca2e5cd867905db7a293a6762dd093
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-19 20:07:23 -0400

    Pre-load S3 data to Lustre filesystem

--- awslinux2/pre-run.sh
+++ awslinux2/pre-run.sh
@@ -1,14 +1,28 @@
 #!/bin/bash -e
 
 prepare_workspace(){
-  
+
   set -o noglob
 
-  echo "copying files into processing directory"
-  cp -R /scratch/results/samples/${OBJECT_NAME} /scratch/results/${AWS_BATCH_JOB_ID}/${AWS_BATCH_JOB_ATTEMPT}/
+  echo "Preloading files from S3..."
+  cd "/scratch/results/samples/${OBJECT_NAME}"
+  find "$(pwd -P)" -type f -exec lfs hsm_restore {} \;
+
+  # wait for restoration...
+  while :; do
+    # https://docs.aws.amazon.com/fsx/latest/LustreGuide/preload-file-contents-hsm.html
+    REMAINING_FILES=$(find "$(pwd -P)" -type f -print0 | xargs -0 -n 1 -P 8 lfs hsm_action | grep "RESTORE" | wc -l)
+    echo "Files remaining: $REMAINING_FILES"
+    [[ $REMAINING_FILES -gt 0 ]] || break
+    echo "$REMAINING_FILES remaining...waiting..."
+    sleep 10
+  done
+
+echo "Preloading filesystem complete."
+  #cp -R /scratch/results/samples/${OBJECT_NAME} /scratch/results/${AWS_BATCH_JOB_ID}/${AWS_BATCH_JOB_ATTEMPT}/
 
 }
 
 # Download inputs
 # Skipping this since we're using Lustre.
-#prepare_workspace
+prepare_workspace
