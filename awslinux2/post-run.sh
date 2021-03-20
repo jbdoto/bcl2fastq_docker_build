b805f44d6fdc9a689ebef79319adceafb7e5ceda
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-19 20:07:45 -0400

    Archive Lustre results post-run

--- awslinux2/post-run.sh
+++ awslinux2/post-run.sh
@@ -1,37 +1,35 @@
 #!/bin/bash -e
 
-upload(){
-  
+archive_files() {
+
   set -o noglob
-
-  local to=$1
-
-  echo "Uploading files post-run..."
-  echo "cd /scratch/results/samples/${OBJECT_NAME}  && aws s3 cp . ${to} --recursive --exclude \"*\" --include \"*.fastq.gz\" --include\"*/Stats/*\" --include \"*IndexMetricsOut.bin\""
-  cd /scratch/results/samples/${OBJECT_NAME} && aws s3 cp . ${to} --recursive --exclude "*" --include "*.fastq.gz"
-  aws s3 cp ./Data/Intensities/BaseCalls/Stats ${to}/Data/Intensities/BaseCalls/Stats --recursive
-  aws s3 cp ./InterOp/IndexMetricsOut.bin ${to}/InterOp/IndexMetricsOut.bin
 
   # https://docs.aws.amazon.com/fsx/latest/LustreGuide/release-files.html
   # release files from lustre to save space:
-  echo "Releasing files from lustre..."
-  # emit full path of file for hsm_release
-  # can only hsm_release files that were cached from S3...so data produced during the run will cause an error.
-  # we therefore filter out fastq, summary, and IndexMetricsOut.bin files.
-  find "$(pwd -P)" -type f ! -name "*.fastq.gz" ! -wholename "*/Stats/*" ! -name "*IndexMetricsOut.bin" -exec lfs hsm_release {} \;
-  echo "Release complete."
+  # Archive all files back to bucket.
+  echo "Archiving files back to S3..."
+  echo "Current directory: $(pwd)"
+  # currently in:  /scratch/results/samples/${OBJECT_NAME}:
+  find "$(pwd -P)" -type f -exec lfs hsm_archive {} \;
+  while :; do
+    # https://docs.aws.amazon.com/fsx/latest/LustreGuide/exporting-files-hsm.html
+    REMAINING_FILES=$(find "$(pwd -P)" -type f -print0 | xargs -0 -n 1 -P 8 lfs hsm_action | grep "ARCHIVE" | wc -l)
+    echo "Files remaining: $REMAINING_FILES"
+    [[ $REMAINING_FILES -gt 0 ]] || break
+    echo "$REMAINING_FILES remaining...waiting..."
+    sleep 10
+  done
 
-  # clean up generated files:
-  echo "cleaning up generated files..."
-  find "$(pwd -P)" -type f -name "*.fastq.gz"  -exec rm -v {} \;
-  find "$(pwd -P)" -type f -wholename "*/Data/Intensities/BaseCalls/Stats/*" -exec rm -v {} \;
-  find "$(pwd -P)" -type f -name "./InterOp/IndexMetricsOut.bin" -exec rm -v {} \;
-  echo "cleanup done."
+  echo "Archive complete."
+
+  # This step frees up space on the Lustre filesytem
+  echo "Releasing files on Lustre filesystem..."
+  find "$(pwd -P)" -type f -exec lfs hsm_release {} \;
+  echo "File release complete."
 
 }
 
-# Upload outputs post-parent job
-upload "s3://${JOBRESULTS_BUCKET}/${OBJECT_NAME}/${AWS_BATCH_JOB_ID}/${AWS_BATCH_JOB_ATTEMPT}/"
+archive_files
 
 # Record execution succeeded in CloudWatch
 if [[ $STATE_MACHINE_NAME ]]; then
