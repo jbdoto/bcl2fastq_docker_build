f259188654dd330f5372e9f5c4ec86ca6e101038
Jeffrey Doto <jeffdoto@amazon.com>
2021-05-06 13:57:37 -0400

    Make build script account and region configurable

--- awslinux2/build.sh
+++ awslinux2/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+ACCOUNT_ID=<enter_id>
+REGION=us-west-2
 
 docker build . -t bcl2fastq
 docker build -f entrypoint.dockerfile . --build-arg BASE_IMAGE=bcl2fastq -t bcl2fastq
-docker tag bcl2fastq 353276416433.dkr.ecr.us-east-1.amazonaws.com/bcl2fastq:1.0.0
-aws ecr get-login-password --region us-east-1 --profile=gsat | docker login --username AWS --password-stdin 353276416433.dkr.ecr.us-east-1.amazonaws.com
-docker push 353276416433.dkr.ecr.us-east-1.amazonaws.com/bcl2fastq:1.0.0
+docker tag bcl2fastq ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/bcl2fastq:1.0.0
+aws ecr get-login-password --region ${REGION} --profile=ryan-gsat | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/bcl2fastq:1.0.0
